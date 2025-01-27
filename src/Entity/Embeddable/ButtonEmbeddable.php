b93d5c7f9021ae82d92cc88b737e9b171363b6cf
Jérôme Engeln <jerome.engeln@adeliom.com>
2025-01-27 15:00:59 +0100

    fix: button embedable namespace
    Signed-off-by: Jérôme Engeln <jerome.engeln@adeliom.com>

--- src/Entity/Embeddable/ButtonEmbeddable.php
+++ src/Entity/Embeddable/ButtonEmbeddable.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace App\Entity\Embeddable;
+namespace Adeliom\SyliusHappyCMSPlugin\Entity\Embeddable;
 
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
