2b95f438c0161f11f483a27a1abac292bf43ac5b
Jérôme Engeln <jerome.engeln@adeliom.com>
2025-06-03 22:12:04 +0200

    feat: add remove orphan
    Signed-off-by: Jérôme Engeln <jerome.engeln@adeliom.com>

--- .makefile/dev.mk
+++ .makefile/dev.mk
@@ -124,7 +124,7 @@ platform:
 	cd ${APP_DIR} && (ENV=$(ENV) docker compose run --rm php composer global config allow-plugins.${PLUGIN_NAME} true)
 	cd ${APP_DIR} && (ENV=$(ENV) docker compose run --rm php composer dump-autoload)
 	rm -rf ${APP_DIR}/composer.lock
-	cd ${APP_DIR} && (ENV=$(ENV) docker compose run --rm php composer require --no-interaction --with-all-dependencies --no-scripts ${PLUGIN_NAME}="*@dev")
+	cd ${APP_DIR} && (ENV=$(ENV) docker compose run --rm php composer require --no-interaction --with-all-dependencies --remove-orphans --no-scripts ${PLUGIN_NAME}="*@dev")
 	cd ${APP_DIR} && (ENV=$(ENV) docker compose run --rm php composer require --dev symfony/maker-bundle --no-scripts)
 	${MAKE} platform_up
 	${MAKE} platform_assets
@@ -188,4 +188,4 @@ bundle_install_test_files:
 	cd ${APP_DIR} && (ENV=$(ENV) docker compose run --rm php bin/console doc:mig:mig -n)
 
 tssds:
-	cd ${APP_DIR} && (ENV=$(ENV) docker compose run --rm php composer require --no-interaction --with-all-dependencies --no-scripts ${PLUGIN_NAME}="*@dev")
+	cd ${APP_DIR} && (ENV=$(ENV) docker compose run --rm php composer require --no-interaction --with-all-dependencies --remove-orphans --no-scripts ${PLUGIN_NAME}="*@dev")
