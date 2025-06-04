97854d82fc6c6d2ddd52eecd1d98c1b711acf617
Jérôme Engeln <jerome.engeln@adeliom.com>
2025-06-04 21:20:53 +0200

    fix: asset compilation
    Signed-off-by: Jérôme Engeln <jerome.engeln@adeliom.com>

--- public/seo-block-type.70c8ae20.js
+++
@@ -1,1 +0,0 @@
-(()=>{"use strict";document.querySelectorAll("[js-seo-block-content]").forEach((function(e){e.querySelector("[js-seo-block-opener]").addEventListener("click",(function(){e.classList.toggle("is-open")}))}))})();
