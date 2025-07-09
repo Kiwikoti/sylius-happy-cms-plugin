0ded8a2246302d0e89fe6a24ff78be2b3e99e85f
Jérôme Engeln <jerome.engeln@adeliom.com>
2025-07-09 11:40:47 +0200

    feat: add helper to find shared block by key
    Signed-off-by: Jérôme Engeln <jerome.engeln@adeliom.com>

--- src/Twig/SharedBlock/SharedBlockExtension.php
+++ src/Twig/SharedBlock/SharedBlockExtension.php
@@ -4,12 +4,22 @@ declare(strict_types=1);
 
 namespace Adeliom\SyliusHappyCMSPlugin\Twig\SharedBlock;
 
+use Adeliom\SyliusHappyCMSPlugin\Entity\SharedBlock\SharedBlockInterface;
+use Adeliom\SyliusHappyCMSPlugin\Factory\CMS\CmsRoutableInterface;
 use Adeliom\SyliusHappyCMSPlugin\Factory\SharedBlock\Helper;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\DependencyInjection\ParameterBag\ParameterBag;
 use Twig\Extension\AbstractExtension;
 use Twig\TwigFunction;
 
 class SharedBlockExtension extends AbstractExtension
 {
+    public function __construct(
+        private readonly EntityManagerInterface $manager,
+        private readonly ParameterBag $parameterBag,
+    ) {
+    }
+
     /**
      * @return TwigFunction[]
      */
@@ -18,6 +28,35 @@ class SharedBlockExtension extends AbstractExtension
         return [
             new TwigFunction('happy_cms_shared_block_render', [Helper::class, 'renderBlock'], ['is_safe' => ['js', 'html'], 'needs_context' => true, 'needs_environment' => true]),
             new TwigFunction('happy_cms_shared_block_assets', [Helper::class, 'includeAssets'], ['is_safe' => ['js', 'html'], 'needs_context' => true, 'needs_environment' => true]),
+            new TwigFunction('happy_cms_get_shared_block_by_key', $this->getByKey(...)),
         ];
+    }
+
+    public function getByKey(string $key): ?int
+    {
+        try {
+            $resourceName = 'sylius_happy_cms.shared_block';
+            $resources = $this->parameterBag->get('sylius.resources');
+            $modelClass = $resources[$resourceName]['classes']['model'] ?? null;
+
+            if (is_null($modelClass) || !is_a($modelClass, SharedBlockInterface::class, true)) {
+                throw new \InvalidArgumentException(sprintf('The resource "%s" must implement "%s".', $resourceName, SharedBlockInterface::class));
+            }
+
+            $repository = $this->manager->getRepository($modelClass);
+
+            if (is_null($repository) || !method_exists($repository, 'getByKey')) {
+                throw new \InvalidArgumentException(sprintf('The resource "%s" repository must have a method "%s".', $resourceName, 'getByKey'));
+            }
+
+            /** @var SharedBlockInterface|null $object */
+            $object = $repository->getByKey($key);
+            if (!is_null($object)) {
+                return $object->getId();
+            }
+            return null;
+        } catch (\Exception $exception) {
+            return null;
+        }
     }
 }
