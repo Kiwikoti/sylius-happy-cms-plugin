560f21639738cb8659849bff44a15aceab17bffe
Jérôme Engeln <jerome.engeln@adeliom.com>
2025-09-17 12:04:53 +0200

    fix: missing command name
    Signed-off-by: Jérôme Engeln <jerome.engeln@adeliom.com>

--- src/Command/InvalidCacheCommand.php
+++ src/Command/InvalidCacheCommand.php
@@ -5,10 +5,15 @@ declare(strict_types=1);
 namespace Adeliom\SyliusHappyCMSPlugin\Command;
 
 use Adeliom\SyliusHappyCMSPlugin\Services\Cmf\RouteRenderService;
+use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
+#[AsCommand(
+    name: 'happycms:cache:invalidate',
+    description: 'Invalidate all routes http cache',
+)]
 class InvalidCacheCommand extends Command
 {
     public function __construct(
