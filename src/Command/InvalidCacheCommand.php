5bb2a484d33cfa11bcd476081e8119516afe8210
Jérôme Engeln <jerome.engeln@adeliom.com>
2025-09-17 12:00:50 +0200

    feat: declare new command to invalidate cache
    Signed-off-by: Jérôme Engeln <jerome.engeln@adeliom.com>

--- src/Command/InvalidCacheCommand.php
+++ src/Command/InvalidCacheCommand.php
@@ -5,15 +5,10 @@ declare(strict_types=1);
 namespace Adeliom\SyliusHappyCMSPlugin\Command;
 
 use Adeliom\SyliusHappyCMSPlugin\Services\Cmf\RouteRenderService;
-use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
-#[AsCommand(
-    name: 'happycms:cache:invalidate',
-    description: '',
-)]
 class InvalidCacheCommand extends Command
 {
     public function __construct(
